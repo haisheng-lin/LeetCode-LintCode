8d5b3493339e8e7ec0bba312590404b1f9c6061f
Lin Haisheng <Lin Haisheng>
2017-02-12 12:05:19 -0700

    Initial commit

---
+++ src/ClassPackage/Interval.java
@@ -0,0 +1,12 @@
+package ClassPackage;
+
+/**
+ * Created by Lin on 2017/2/12.
+ */
+public class Interval {
+    public int start, end;
+    public Interval(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ src/ClassPackage/ListNode.java
@@ -0,0 +1,10 @@
+package ClassPackage;
+
+/**
+ * Created by Lin on 2017/2/12.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {val = x;}
+}
